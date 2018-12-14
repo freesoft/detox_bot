10bb8efb0ca1e3120fad45b0b9d596663d5c4ec8
freesoft <wonhee.jung@gmail.com>
2018-12-14 12:18:13 -0800

    updated webapp and requirementsd

--- webapp.py
+++ webapp.py
@@ -1,19 +1,26 @@
+# following three lines should have come first than any other imports.
 import gevent.monkey
-gevent.monkey.patch_all()
-
-from flask import Flask
-from flask import render_template
-from flask_socketio import SocketIO
+gevent.monkey.patch_all(subprocess=True)
 
 import json
+import os
+import gevent
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 from detox_engine import ToxicityClassifier
+from joblib import load
+import constant
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'asdf1234'
 socketio = SocketIO(app)
 
-toxicityClassifier = ToxicityClassifier()
+@app.before_first_request
+def load_module():
+    print("initiating before first request...")
+    # do nothing for now
 
 @app.route('/')
 def sessions():
@@ -26,8 +33,11 @@ def messageReceived(methods=['GET', 'POST']):
 
 @socketio.on('my event')
 def handle_my_custom_event(msg, methods=['GET', 'POST']):
+
+    toxicityClassifier = ToxicityClassifier()
+
     print('received my event: username:' + msg['username'] + ', message:' + msg['message'] )
-    print('chat toxicity analyzed...' + str(toxicityClassifier.isToxic(msg['message'])))
+    print('chat toxicity analyzed...' + str(toxicityClassifier.isToxic( msg['message'] )))
     if toxicityClassifier.isToxic(msg['message']) == True:
         socketio.emit('my response', { 'username': msg['username'], 'message': msg['message'], 'is_toxic': '1'}, callback=messageReceived)
     else:
@@ -35,4 +45,4 @@ def handle_my_custom_event(msg, methods=['GET', 'POST']):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, debug=True, host='0.0.0.0', port=port)
+    socketio.run(app, debug=False, host='0.0.0.0', port=port)
